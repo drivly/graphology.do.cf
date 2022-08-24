baed19b283e987e4a03ae4e3d5bfb128bf26751c
Scott Olson <solson@zebra.com>
2022-08-24 12:50:10 -0500

    Initial implementation - updated README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# graphology.do.cf
-A Graphology Implementation with Cloudflare Workers, KV, and Durable Objects
+A [Graphology](https://graphology.github.io/) Implementation using Cloudflare Worker Durable Objects.
+
+```json
+{
+    "url": "https://my.workers.dev/myGraph",
+    "key": "/myGraph",
+    "nodes": 0,
+    "edges": 0,
+    "graph": {
+        "options": {
+            "type": "mixed",
+            "multi": false,
+            "allowSelfLoops": true
+        },
+        "attributes": { },
+        "nodes": [ ],
+        "edges": [ ]
+    }
+}
+```
+
+## Options
+
+- `?newNode=nodeId|{"jsonAttr": "jsonValue"}` creates a new node with the ID and JSON value(s) provided.
+- `?newEdge=nodeId-nodeId` creates a new edge between nodes with the IDs provided.
+- `?clear` clears the graph of all nodes and edges.
+
+## [🚀 We're hiring!](https://careers.do/apply)
+[Driv.ly](https://driv.ly) is simple APIs to buy & sell cars online, funded by some of the [biggest names](https://twitter.com/TurnerNovak) in [automotive](https://fontinalis.com/team/#bill-ford) and [finance & insurance](https://www.detroit.vc)
+
+We're building our entire infrastructure on Cloudflare Workers, Durable Objects, KV, R2, and PubSub.  If you're as passionate about these transformational technologies as we are, we'd love for you to join our rapidly-growing team.
---
+++ worker.js
@@ -0,0 +1,60 @@
+import Graph from 'graphology'
+
+export default {
+  fetch: (req, env) => {
+    // get durable object
+    // const ip = req.headers.get('cf-connecting-ip')
+    const { hostname, pathname } = new URL(req.url)
+    const stub = env.GRAPH.get(env.GRAPH.idFromName(hostname + pathname))
+    
+    // fetch durable object
+    return stub.fetch(req)
+  }
+}
+
+export class DataGraph {
+  constructor(state, env) {
+    this.state = state;
+    // `blockConcurrencyWhile()` ensures no requests are delivered until
+    // initialization completes.
+    this.state.blockConcurrencyWhile(async () => {
+      let stored = await this.state.storage.get("value");
+      // After initialization, future reads do not need to access storage.
+      this.graph = stored || new Graph();
+    });
+  }
+
+  // Handle HTTP requests from clients.
+  async fetch(req) {
+    const url = new URL(req.url)
+
+    for (const [key, value] of url.searchParams.entries()) {
+      console.log(`${key}, ${value}`);
+      switch(key) {
+        case 'newNode':
+          const nAttrs = value.split('|');
+          nAttrs.length == 1 ? this.graph.addNode(value) : this.graph.addNode(nAttrs[0], JSON.parse(nAttrs[1]));
+          break;
+        case 'newEdge':
+          const eNodes = value.split('-');
+          console.log(eNodes);
+          this.graph.addEdge(eNodes[0], eNodes[1]);
+          this.graph.addEdge(eNodes[1], eNodes[0]);
+          break;
+        case 'clear':
+          this.graph.clear();
+      }
+   }
+
+    // store the updated graph
+    await this.state.storage.put("value", this.graph)
+
+    return new Response(JSON.stringify({
+      url: url.origin + url.pathname,
+      //key: url.pathname.replace('/api/',''),
+      nodes: this.graph ? this.graph.order : 0,
+      edges: this.graph ? this.graph.size : 0,
+      graph: this.graph ?? null
+    }, null, 2), { headers: { 'content-type': 'application/json' }})
+  }
+}
